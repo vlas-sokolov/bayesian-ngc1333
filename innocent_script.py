019b1f93caa203c5db8777742ed6c10b333e5bed
Vlas Sokolov <vlas145@gmail.com>
2018-12-07 09:58:22 +0100

    Better (wider) priors, add some more comments

--- innocent_script.py
+++ innocent_script.py
@@ -1,3 +1,12 @@
+"""
+The main sampling script.
+
+Takes pixel positions (x, y) and the number of components (npeaks)
+from the command line (via sys.argv[]), sets the priors, the likelihood
+function, preforms some sanity checks, and fires up MultiNest through
+pymultinest.
+"""
+
 import os
 import sys
 import mpi4py
@@ -6,7 +15,7 @@ import numpy as np
 from astropy import log
 import astropy.units as u
 from astropy.io import fits
-# those import warnings are annoying
+# Those import warnings are annoying
 with warnings.catch_warnings():
     warnings.simplefilter('ignore')
     #import corner
@@ -16,18 +25,17 @@ with warnings.catch_warnings():
     from pyspecnest.ammonia import get_nh3_model, get_corner_kwargs
     from pyspeckit.spectrum.models.ammonia_constants import freq_dict
     from pyspecnest.chaincrunch import pars_xy, lnZ_xy, get_zero_evidence
-# all the i/o functions now reside here
+# all the I/O functions now reside here
 import opencube
 
 # NOTE: change these lines into input arguments and you're good to go
-#       Don't forget to edit the priors and the cube i/o!
+#       Don't forget to edit the priors and the cube I/O!
 lines = ['nh311', 'nh322']
 line_names = ['oneone', 'twotwo']
 npars = 6
 npeaks = 2
 name_id = 'ngc1333-gas'
 proj_dir = os.path.expanduser('~/Projects/ngc1333-gas/')
-
 
 # need the arguments below to save / load spectra
 # (making a SpectralCube each time is too slow!)
@@ -39,7 +47,6 @@ iokwargs = dict(
     target_errfile='{}-errors.npy'.format(name_id),
     target_header='{}-header.p'.format(name_id),
     mmap_mode='r')
-
 
 n_live_points = 400
 sampling_efficiency = 0.8
@@ -107,43 +114,45 @@ opencube.update_model(sp, fittype_fmt.format(npeaks))
 # needed because of this:
 # https://github.com/pyspeckit/pyspeckit/issues/179
 sp.specfit.fitter.npeaks = npeaks
-# npeaks > 1 seems to break beacause of fitter.parnames is not mirrored
+# npeaks > 1 seems to break because of fitter.parnames is not mirrored
 if len(sp.specfit.fitter.parnames)==npars and npeaks > 1:
     sp.specfit.fitter.parnames *= npeaks
 
 # special considerations on the priors:
-#   - Line width: we're going to follow Henshaw et al. (2014) here and
-#     set the minimal FWHM to be relocity resolution, 0.2 km/s that is.
+#     - Temperatures: without a better knowledge, setting the priors to be as
+#                     wide as we expect them to be - T_CMB to 25 (upper bound
+#                     from the fact that we probably can't constrain it much
+#                     further from the ammonia level population)
+#     - Line widths: set the minimal FWHM to be velocity resolution, 0.07 km/s
 sig2fwhm = 2*(2*np.log(2))**0.5
-min_sigma = 0.2 / sig2fwhm
-#   - Total ammonia column: ranging from dex 14 to 16, for a typical
-#     NH3 abundance of 1e-8 we're tracing H2 densities of 1e22 to 1e24
+min_sigma = 0.07 / sig2fwhm
+#   - Total ammonia column: ranging from dex 12 to 15, for a typical NH3
+#       abundance of 1e-8 we're tracing H2 densities of 1e20 to 1e23
 #   - Vlsr - from prior knowledge of cloud kinematics
-
-dv_min = 0.2 # minimum separaton of the velocity components
-dv_max = 3.0 # maximum velocity component spacing
+dv_min, dv_max = 0.2, 3.0 # min/max separation of the velocity components
 dxoff_prior = [dv_min, dv_max]
 priors_xoff_transformed = (([[2.7315, 25], [2.7315, 25], [12.0, 15.0],
-           [min_sigma, 1], dxoff_prior, [0.05, 1]])[::-1] * (npeaks - 1)
+           [min_sigma, 1], dxoff_prior, [0.05, 2]])[::-1] * (npeaks - 1)
                           + [[2.7315, 25], [2.7315, 25], [12.0, 15.0],
-           [min_sigma, 1], [5, 10], [0.05, 1]][::-1])
-# FIXME: make a wrapper function for pyspecnest instead!
+           [min_sigma, 1], [3, 10], [0.05, 1]][::-1])
+# TODO: make a wrapper function for pyspecnest instead!
 priors = priors_xoff_transformed
 
 nh3_model = get_nh3_model(sp, ['oneone', 'twotwo'], sp.error,
-                          priors=priors, npeaks=npeaks)
+                          priors=priors, npeaks=npeaks, restrict_tex=True)
 
-# safeguard
+# Safeguard - check some common causes of failure before scheduling
+# a job that would just throw tens of thousands of errors at us
 no_valid_chans = not np.any(np.isfinite(nh3_model.ydata))
 sanity_check = np.isfinite(
     nh3_model.log_likelihood([15, 5, 15, 0.2, 7, 0.5] * npeaks,
                              nh3_model.npars, nh3_model.dof))
 if no_valid_chans or not sanity_check:
-    # NOTE: sanity chack will fail if, e.g., the errors are not finite
+    # This should fail if, e.g., the errors are not finite
     log.error("no valid pixels at x={}; y={}. Aborting.".format(*yx[::-1]))
-    import sys
     sys.exit()
 
+# The first process gets to make the directory structure!
 output_dir = os.path.join(proj_dir, 'nested-sampling/')
 fig_dir = os.path.join(output_dir, 'figs/')
 suffix = 'x{1}y{0}'.format(*yx)
@@ -158,13 +167,15 @@ if not os.path.exists(chains_dir):
 
 chains_dir = '{}/{}-'.format(chains_dir, npeaks)
 
-# fire up the multinest blackbox
+# Run MultiNest on the model+priors specified
 pymultinest.run(nh3_model.xoff_symmetric_log_likelihood,
                 nh3_model.prior_uniform, nh3_model.npars,
                 outputfiles_basename=chains_dir,
                 verbose=True, n_live_points=n_live_points,
                 sampling_efficiency=sampling_efficiency)
 
+# The remainder of the script is not essential for sampling, and can be safely
+# moved out into a script of its own.
 if i_am_root() and plot_fit:
     # parse the results as sensible output
     from pyspecnest.chaincrunch import analyzer_xy
@@ -188,7 +199,6 @@ if i_am_root() and plot_fit:
                                         Zs[npeaks] - Zs[npeaks-1]))
     if npeaks > 1:
         log.info('ln(Z{}/Z{}) = {:.2f}'.format(npeaks, 0, Zs[npeaks] - Zs[0]))
-
 
 if plot_fit and i_am_root():
     sp.plotter(errstyle='fill')
